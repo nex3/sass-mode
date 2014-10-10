26a66e331b507fb420e3bb7d0a6a8fbb04294343
Steve Purcell <steve@sanityinc.com>
2014-10-10 20:36:41 +0100

    Revert "Don't override the syntax classes for "-" and "_""
    This reverts commit d19542f4131b3e6d94655ab6c7403442bf2cc332.

--- sass-mode.el
+++ sass-mode.el
@@ -78,7 +78,10 @@ text nested beneath them.")
     (,(regexp-opt '("and" "or" "not")) 0 font-lock-keyword-face)))
 
 (defconst sass-syntax-table
-  (make-syntax-table))
+  (let ((st (make-syntax-table)))
+    (modify-syntax-entry ?- "w" st)
+    (modify-syntax-entry ?_ "w" st)
+    st))
 
 (defconst sass-script-syntax-table
   (let ((st (make-syntax-table sass-syntax-table)))
